db555b9f14cc11b420b51042fa901defd48e7217
otrack <0track@gmail.com>
2014-11-17 01:00:06 +0100

    [ADD] filterUtils

---
+++ src/main/java/org/apache/nutch/util/FilterUtils.java
@@ -0,0 +1,43 @@
+package org.apache.nutch.util;
+
+import org.apache.gora.filter.FilterOp;
+import org.apache.gora.filter.MapFieldValueFilter;
+import org.apache.nutch.storage.Mark;
+import org.apache.nutch.storage.WebPage;
+
+import static org.apache.nutch.metadata.Nutch.ALL_CRAWL_ID;
+import static org.apache.nutch.parse.ParserJob.REPARSE;
+
+/**
+ *
+ * @author Pierre Sutra
+ */
+public class FilterUtils {
+
+  public static MapFieldValueFilter<String, WebPage> getBatchIdFilter(
+    String batchId, Mark mark) {
+    if (batchId.equals(REPARSE)
+      || batchId.equals(ALL_CRAWL_ID)) {
+      return null;
+    }
+    MapFieldValueFilter<String, WebPage> filter = new MapFieldValueFilter<>();
+    filter.setFieldName(WebPage.Field.MARKERS.toString());
+    filter.setFilterOp(FilterOp.EQUALS);
+    filter.setFilterIfMissing(true);
+    filter.setMapKey(mark.getName());
+    filter.getOperands().add(batchId);
+    return filter;
+  }
+
+  public static MapFieldValueFilter<String, WebPage> getExcludeAnyBatchIdFilter(
+    Mark mark){
+    MapFieldValueFilter<String, WebPage> filter = new MapFieldValueFilter<>();
+    filter.setFieldName(WebPage.Field.MARKERS.toString());
+    filter.setFilterOp(FilterOp.UNLIKE);
+    filter.setFilterIfMissing(false);
+    filter.setMapKey(mark.getName());
+    filter.getOperands().add("*");
+    return filter;
+  }
+
+}
